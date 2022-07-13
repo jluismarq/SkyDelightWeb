2e3414ebcc92ab23fed462ec946606294b2a78d1
Jorge Luis Márquez León <jmarquezl1200@alumno.ipn.mx>
2022-07-12 20:20:58 -0500

    Cambiando diseño

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -4,46 +4,46 @@ import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Checkbox from '@mui/material/Checkbox';
-import FormGroup from '@mui/material/FormGroup';
 import Button from '@mui/material/Button';
 import FormControlLabel from '@mui/material/FormControlLabel';
-
+import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
 
 const LoginView = ( ) => {
+
     const [login,setLogin]=React.useState({email:"",password:""})
     const handleEvent=(event)=>{setLogin(previewLogin=>({...previewLogin,[event.target.id]:event.target.value}))}
     const submit = () =>{console.log(login)}
+
     return (
-        <Grid container id="main-container" justifyContent="center" direction="column" alignItems="center" sx={{marginTop:{xs:"10em", md:"5em"}}}>
+        <Grid container id="main-container">
+            <Paper elevation = {20} style={{padding:"50px", height:"60vh", width:"280px", margin:"50px auto"}}>
 
-        <Grid item  md={4}>
-            <img className="logo" src={logo} alt="SkyDelight" />
-            <h1>Iniciar Sesión</h1>
-        </Grid>
+                <Grid item align='center'>
+                    <img className="logo" src={logo} alt="SkyDelight" />
+                    <h2>Iniciar Sesión</h2>
+                </Grid>
 
-        
-        <Grid item>
-            <FormGroup sx={{padding:"10px"}}>
-                <TextField fullWidth required type="email" id="email" value={login.email} onChange={handleEvent} label="Email" variant="outlined" />
-                <TextField fullWidth id="password" value={login.password} onChange={handleEvent} label="Contraseña" type="password" autoComplete="current-password" />
-                <FormControlLabel sx={{margin:"0 auto"}} control={<Checkbox defaultChecked />} label="Mantener sesión iniciada" />
-            </FormGroup>  
-        
-        <Grid item>
-            <Button fullWidth variant="contained" onClick={submit}>Iniciar Sesión</Button>
+                
+                <Grid item>
+                    <TextField fullWidth required type="email" id="email" value={login.email} onChange={handleEvent} label="Email" placeholder='Ingrese su correo' variant="outlined" margin="dense" />
+                    <TextField fullWidth required id="password" value={login.password} onChange={handleEvent} label="Contraseña" placeholder='Ingrese su contraseña' type="password" autoComplete="current-password" margin="dense" />
+                    <FormControlLabel sx={{margin:"0 auto"}} control={<Checkbox defaultChecked />} label="Mantener sesión iniciada" /> 
+                </Grid>
+                
+                <Button fullWidth variant="contained" onClick={submit} style={{margin:"8px"}}>Iniciar Sesión</Button>
+              
+                <Typography style={{margin:"8px"}}>
+                    <Link href="#" underline="hover">¿Ólvidaste tu contraseña?</Link>
+                </Typography>   
 
-        </Grid>
-          
-        <Typography variant="overline" display="block" gutterBottom>
-          ¿Ólvido su contraseña? o Regístrate
-        </Typography>
-         
+               <Typography style={{margin:"8px"}}> 
+                    <Link href="#" underline="hover">¿No tienes cuenta?</Link> 
+               </Typography>
+               
 
-        </Grid>
-
-     
-       
-
+                   
+            </Paper>
         </Grid>
       );
 }
